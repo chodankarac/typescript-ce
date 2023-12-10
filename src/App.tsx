2c06a5faf6f40cdf48d04768786a77a2f8960634
ashish.chodankar <ashish.chodankar@squareyards.co.in>
2023-12-10 09:56:16 +0530

    passing event props;implements issue #5

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import "./App.css";
+import { Button } from "./components/Button";
 import { Greet } from "./components/Greet";
 import Heading from "./components/Heading";
+import { Input } from "./components/Input";
 import { Oscar } from "./components/Oscar";
 import { Status } from "./components/Status";
 
@@ -13,6 +15,12 @@ function App() {
         <Heading>Oscar goes to Leonardo Dicapario!</Heading>
       </Oscar>
       <Greet name="Ashish" isLoggedIn={false} />
+      <Button
+        handleClick={(event, id) => {
+          console.log("button", event, id);
+        }}
+      />
+      <Input value="" handleChange={(event) => console.log(event)} />
     </div>
   );
 }
